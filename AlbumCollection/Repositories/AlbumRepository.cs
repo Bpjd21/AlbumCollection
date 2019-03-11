c2687f743cd8194ba8c434e7c4b3f094144472c3
Bpjd21 <bpjd21@yahoo.com>
2019-03-10 23:00:48 -0400

    Added more files

--- AlbumCollection/Repositories/AlbumRepository.cs
+++ AlbumCollection/Repositories/AlbumRepository.cs
@@ -1,9 +1,7 @@
 ﻿using AlbumCollection.Models;
-using NSubstitute.Core;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace AlbumCollection.Repositories
 {
--- AlbumCollection/Repositories/SongRepository.cs
+++ AlbumCollection/Repositories/SongRepository.cs
@@ -1,8 +1,6 @@
 ﻿using AlbumCollection.Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace AlbumCollection.Repositories
 {
